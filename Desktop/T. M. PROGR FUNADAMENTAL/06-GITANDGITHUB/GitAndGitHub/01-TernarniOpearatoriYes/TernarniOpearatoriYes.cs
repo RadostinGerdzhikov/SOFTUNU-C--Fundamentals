d7ba65c041470aabd3424a394cb08a11e184c3e6
platnoto <rgerdzhikov@gmail.com>
2018-01-26 17:20:37 +0200

    Add Likanka

---
+++ Desktop/T. M. PROGR FUNADAMENTAL/06-GITANDGITHUB/GitAndGitHub/01-TernarniOpearatoriYes/TernarniOpearatoriYes.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+
+class TernarniOpearatoriYes
+{
+    static void Main()
+    {
+
+   bool n = bool.Parse(Console.ReadLine());
+        bool b = bool.Parse(Console.ReadLine());
+        bool answer;
+
+        // condition? true_expression : false_expression;
+        
+        // ?: conditional operator.  
+        n=b ? "yes" : "no";
+        // neshtp ne stava
+        // shet gledam da vidq kak stava
+
+
+
+
+
+    }
+}
+
