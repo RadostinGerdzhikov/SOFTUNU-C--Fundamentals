91ea4bb24007ad7271af957b31dcf539e3d5b030
rrstefanov <rrstefanov@hotmail.com>
2018-03-11 14:47:49 +0200

    Add the project

---
+++ 01. Debit Card Number/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _01._Debit_Card_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 03. Miles to Kilometers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _03._Miles_to_Kilometers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
