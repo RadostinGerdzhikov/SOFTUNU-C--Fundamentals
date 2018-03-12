c79059292872082a0ec5f13482fc4a359f6dcd76
RadostinGerdzhikov <rgerdzhikov@gmail.com>
2018-03-12 14:26:02 +0200

    1.Update Readme.md -Add description problem 4. 2 Delete some folders. Evry contributor must add only one folder with desicion of one problem fromExercises: C# Intro and Basic Syntax in folder intro-and-basic-syntax-exercise. 3 Document solution of Problem 3. 3 Add solution problem 2. 4.Update team.txt and add personal githUsernam and soft uni username

---
+++ Desktop/TechModule/02aGitHubHomerk!/Teamwork/programming-fundamentals/intro-and-basic-syntax-exercise/03. Miles to Kilometers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Milescalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+		   //We recieve from the Console the miles -> a double
+            var miles1 = double.Parse(Console.ReadLine());
+            
+			
+			// We calculate miles in kilometers with formula from Internet shows that one mile is equal 1.60934 km.
+            var km1 = miles1 * 1.60934;
+            
+			// We print the finish result form program
+            Console.WriteLine("{0:F2}", km1);
+
+        }
+    }
+}
