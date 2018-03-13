bc32a3e4f8fc60f7958c8b815a51a5ee6aab7193
RadostinGerdzhikov <rgerdzhikov@gmail.com>
2018-03-13 05:37:33 +0200

    Update repo by condition!

---
+++ Desktop/TechModule/WorkTEAM/programming-fundamentals/intro-and-basic-syntax-exercise/02.Rectangle Area/02.RectangleArea.cs
@@ -0,0 +1,15 @@
+﻿namespace _02.RectangleArea
+{
+    using System;
+
+    public class RectangleArea
+    {
+        public static void Main()
+        {
+            var n = double.Parse(Console.ReadLine());
+            var b = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{(n * b):f2}");
+        }
+    }
+}
